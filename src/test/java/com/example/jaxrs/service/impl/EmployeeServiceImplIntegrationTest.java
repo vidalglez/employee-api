11f514ffa40ff617bd7f92f4afda6dcc21f5471b
Vidal Gonzalez <darkvidal@gmail.com>
2019-10-15 21:25:57 -0500

    test: added get employees

--- src/test/java/com/example/jaxrs/service/impl/EmployeeServiceImplIntegrationTest.java
+++ src/test/java/com/example/jaxrs/service/impl/EmployeeServiceImplIntegrationTest.java
@@ -1,68 +1,66 @@
 package com.example.jaxrs.service.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
+import org.mockito.Spy;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.jaxrs.model.Employee;
 import com.example.jaxrs.repository.EmployeeRepository;
 import com.example.jaxrs.service.EmployeeService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
-//@RunWith(MockitoJUnitRunner.class)
 public class EmployeeServiceImplIntegrationTest {
 	
-	@Autowired
-	//@MockBean
-	//@InjectMocks
 	private EmployeeService service;
 	
-	@MockBean(name = "employeeRepository")
-	//private EmployeeRepository employeeRepository;
-	static private EmployeeRepository employeeRepository;
+	@Spy
+	private EmployeeRepository employeeRepository;
 	
-	@TestConfiguration
-	static class EmployeeServiceImplTestContextConfiguration{
-		
-		@Bean
-		public EmployeeService employeeService() {
-			//return Mockito.mock(EmployeeServiceImpl.class);
-			return new EmployeeServiceImpl(employeeRepository);
-		}
-		
-		@Bean
-		public ObjectMapper mapper() {
-			return Mockito.mock(ObjectMapper.class);
-			//return new ObjectMapper();
-		}
-	}
-	
+	private List<Employee> employees;
+
 	
 	@Before
 	public void setUp() {
-		LocalDate birtdDate = LocalDate.of(1959, 8, 11);
-		LocalDate doe = LocalDate.of(2006, 10, 10);
-		Employee emp = new Employee(1, "Gustavo","Master", "Cerati", birtdDate, doe, true);
+		service = new EmployeeServiceImpl(employeeRepository);
 		
-		Mockito.when(employeeRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(emp));
+		
+		Employee[] emps = { 
+			new Employee(1, "Gustavo","Master", "Cerati", LocalDate.of(1959, 8, 11), LocalDate.of(2006, 10, 10), true),
+			new Employee(2, "Enrique","Master", "Bunbury", LocalDate.of(1965, 8, 11), LocalDate.of(1997, 8, 21), true),
+			new Employee(3, "Rafa","Mariachi", "Dominguez", LocalDate.of(1971, 3, 9), LocalDate.of(1999, 5, 17), false),
+			new Employee(4, "David","Master", "Gilmour", LocalDate.of(1951, 12, 1), LocalDate.of(1976, 4, 20), true)
+			
+		};
+		employees = new ArrayList<>(Arrays.asList(emps));
+		
 	}
 	
 	@Test
 	public void testGetEmployee() {
+		Mockito.when(employeeRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(employees.get(0)));
 		Employee employee = service.getEmployee(1);
-		//assertNull(employee);
+		assertThat("Gustavo").isEqualTo( employee.getFirstName());
+		assertThat("Cerati").isEqualTo(employee.getLastName());
+		assertThat(true).isEqualTo(employee.getStatus());
 		assertNotNull(employee);
+	}
+	
+	@Test
+	public void testGetEmployees() {
+		Mockito.when(employeeRepository.findAll()).thenReturn(employees);
+		List<Employee> employeesList = service.getEmployees();
+		assertThat(employeesList.size()).isEqualTo(3);
 	}
 }
